c5ddd211a3b1743d7b2a6988e44da539906cf357
Juan <juanbb1505@gmail.com>
2021-11-12 02:26:33 -0500

    app load

---
+++ client/build/precache-manifest.0b9b07e6cf1d850e0be9307287487478.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddb49213e875e8310decd730ea6d3f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "add1b3e99f81ef42a810",
+    "url": "/static/css/main.5a3df583.chunk.css"
+  },
+  {
+    "revision": "03342e511f094137f868",
+    "url": "/static/js/2.9119e130.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.9119e130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add1b3e99f81ef42a810",
+    "url": "/static/js/main.4b4ccaa5.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
